2ac37d08af28ce0be0a9e916b9e6be466efc2f01
Derek Ekins <derek@spathi.com>
2019-11-10 10:03:41 +0000

    tidied up index and copyrights

--- frontend/packs/initiatives_map.js
+++ frontend/packs/initiatives_map.js
@@ -16,9 +16,8 @@ function initialiseMap(initiatives) {
     {
       maxZoom: 18,
       attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        '<a href="https://www.mapbox.com/" target="_blank">&copy; Mapbox</a> ' +
+        '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap</a>',
       id: "mapbox.streets"
     }
   ).addTo(carbonExplorer);
