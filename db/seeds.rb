f5906459aea748700642ad2d774afc4a16049ec4
Derek Ekins <derek@spathi.com>
2020-01-13 09:54:23 +0000

    updated initiative status and group type seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,20 @@
 #
 # Examples:
 
-InitiativeStatus.create! name: 'Initiation',
-                         description:
-                           'This could cover feasibility, initial mobilisation, brainstorming, etc.'
-InitiativeStatus.create! name: 'Planning',
-                         description:
-                           'This assumes a core team assembled and feasibility issues resolved, and now preparing a plan or finding funding or similar activities.'
-InitiativeStatus.create! name: 'Implementation',
-                         description:
-                           'This is where the project is being implemented and may include a number of project sub-stages. A complex project would benefit from having a full work break-down structure for this stage.'
-InitiativeStatus.create! name: 'Operational',
-                         description:
-                           'For an operational venture, this is the hand-over from project mode to an operations mode for the venture in question. For some initiatives, they may be broken into PHASES so the completion of one phase, would precede the INITIATION of another phase.'
+InitiativeStatus.create! name: 'In planning',
+                         description: 'This covers the period after an idea has been formed and initial plans have been prepared, but before the initiative is ready to execute plans (team, funding, resources, permissions, designs, etc. in place).'
+
+InitiativeStatus.create! name: 'In planning',
+                         description: 'This covers the period where a detailed plan to implement the initiative has turned into action for an initiative: conducting a survey; building a community hall solar array; creating a community gardens service; or whatever. After this, an initiative will either be Running, Completed or Abandoned.'
+
+InitiativeStatus.create! name: 'Running',
+                         description: 'After creation of a service or operation, this indicates that it is running and will remain so indefinitely.'
+
 InitiativeStatus.create! name: 'Completed',
-                         description:
-                           'For a one off project this involves the closure of the project, during which a debrief and gathering of lessons would help in communicating results to others wishing to execute a similar project. For an operational venture, this is the hand-over from project mode to an operations mode for the venture in question. For some initiatives, they may be broken into PHASES so the completion of one phase, would precede the INITIATION of another phase.'
+                         description: 'If an initiative is neither a service nor operation - such as a one off survey or insulation project for a street - then once done, it moves to a completed state. End of story.'
+
 InitiativeStatus.create! name: 'Abandoned',
-                         description:
-                           'A project that has been abandoned is worth flagging, as we could learn lessons from it (was it lack of critical mass in uptakes? lack of trained resources? funding difficulties? … This status won’t say why, but maybe a chat with the Initiative owner could be useful to someone trying the start a similar initiatives.'
+                         description: 'An initiative that has been abandoned is worth flagging, as we could learn lessons from it. Either an In development initiative or a Running one could be Abandoned.'
 
 GroupType.create! name: 'Association'
 GroupType.create! name: 'Charity'
@@ -33,6 +29,7 @@ GroupType.create! name: 'Limited Company'
 GroupType.create! name: 'Partnership'
 GroupType.create! name: 'Public Body'
 GroupType.create! name: 'Trust'
+GroupType.create! name: 'Other'
 
 User.create! email: 'admin@test.com', password: 'password', role: 'admin'
 User.create! email: 'dev@test.com', password: 'password', role: 'consumer'
