2cc5a8ce6b30792c3285c84a162579da450b0851
Derek Ekins <derek@spathi.com>
2021-04-16 17:04:02 +0100

    add publish hint

--- app/helpers/initiative_helper.rb
+++ app/helpers/initiative_helper.rb
@@ -7,12 +7,13 @@ module InitiativeHelper
   end
 
   # rubocop:disable Metrics/ParameterLists
-  def initiative_step(number, current, title:, next_text:, intro_text: nil, &block)
+  def initiative_step(number, current, title:, next_text:, next_suffix_text: nil, intro_text: nil, &block)
     return unless current == number
 
     render('partials/initiative_step',
            title: title,
            current: current,
+           next_suffix_text: next_suffix_text,
            next_text: next_text,
            intro_text: intro_text,
            step: number, &block)
